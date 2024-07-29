3f904dc4d00fd377e727bd1b3cada28eed90cc2c
lst97 <laisiotou1997@gmail.com>
2024-07-29 19:17:22 +1000

    update: docker config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -48,7 +48,7 @@ class AppConfig implements IAppConfig {
 		this.host = configData.host;
 		this.apiVersion = configData.apiVersion;
 		this.apiEndpoint = configData.apiEndpoint;
-		this.database = process.env.MONGODB_URI ?? configData.database;
+		this.database = configData.database;
 		this.certificates = configData.certificates;
 		this.appIdentifier = configData.appIdentifier;
 		this.environment = configData.environment;
